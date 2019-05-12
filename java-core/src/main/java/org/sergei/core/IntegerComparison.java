3a298778b423136aad91c7663fead0665f4c78bd
Sergei Visotsky <sergei.visotsky@gmail.com>
2019-05-12 21:54:01 +0300

    More Java stuff

---
+++ java-core/src/main/java/org/sergei/core/IntegerComparison.java
@@ -0,0 +1,22 @@
+package org.sergei.core;
+
+public class IntegerComparison {
+    public static void main(String[] args) {
+        Integer a = 300;
+        Integer b = 300;
+
+        if (a == b) {
+            System.out.println("yes");
+        } else {
+            System.out.println("no");
+        }
+        System.out.println("Value equality check");
+        if (a.equals(b)) {
+            System.out.println("yes");
+        } else {
+            System.out.println("no");
+        }
+    }
+}
+
+
