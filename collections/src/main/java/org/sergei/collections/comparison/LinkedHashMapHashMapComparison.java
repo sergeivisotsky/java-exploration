ff5dd2d6c502c42fe379270665416d4f7e12b323
Sergei Visotsky <sergei.visotsky@gmail.com>
2019-05-18 07:58:51 +0300

    Comparison update

--- collections/src/main/java/org/sergei/collections/comparison/LinkedHashMapHashMapComparison.java
+++ collections/src/main/java/org/sergei/collections/comparison/LinkedHashMapHashMapComparison.java
@@ -3,38 +3,35 @@ package org.sergei.collections.comparison;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Random;
 
 /**
- * LinkedHashMap works quicker than HashMap
+ * HashMap works quicker than HashMap
  */
 public class LinkedHashMapHashMapComparison {
 
     public static void main(String[] args) {
         System.out.println("HashMap:");
         long beforeMap = System.nanoTime();
-        Map<Integer, String> fruitsMap = new HashMap<>();
-        fruitsMap.put(1, "apple");
-        fruitsMap.put(2, "orange");
-        fruitsMap.put(3, "mellon");
-        fruitsMap.put(4, "grape");
-        fruitsMap.put(5, "quiwi");
-        fruitsMap.forEach((k, v) -> System.out.println("Key: " + k + " Value: " + v));
+        Map<Integer, Integer> elemMap = new HashMap<>();
+        for (int i = 0; i < 100_000; i++) {
+            elemMap.put(i, new Random().nextInt());
+        }
+//        elemMap.forEach((k, v) -> System.out.println("Key: " + k + " Value: " + v));
         long afterMap = System.nanoTime();
-        System.out.println("Performance: " + (afterMap - beforeMap));
+        System.out.println("Performance: " + (afterMap - beforeMap)); // This guy works quicker
         
         System.out.println();
 
         System.out.println("LinkedHashMap:");
         long beforeLHM = System.nanoTime();
-        Map<Integer, String> fruitsLHM = new LinkedHashMap<>();
-        fruitsLHM.put(1, "apple");
-        fruitsLHM.put(2, "orange");
-        fruitsLHM.put(3, "mellon");
-        fruitsLHM.put(4, "grape");
-        fruitsLHM.put(5, "quiwi");
-        fruitsLHM.forEach((k, v) -> System.out.println("Key: " + k + " Value: " + v));
+        Map<Integer, Integer> elemLHM = new LinkedHashMap<>();
+        for (int i = 0; i < 100_000; i++) {
+            elemLHM.put(i, new Random().nextInt());
+        }
+//        elemLHM.forEach((k, v) -> System.out.println("Key: " + k + " Value: " + v));
         long afterLHM = System.nanoTime();
-        System.out.println("Performance: " + (afterLHM - beforeLHM)); // This guy works quicker
+        System.out.println("Performance: " + (afterLHM - beforeLHM));
     }
     
 }
