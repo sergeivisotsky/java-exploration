c4ba83bd8f99354460b3189389893518104b920c
Sergei Visotsky <sergei.visotsky@gmail.com>
2019-06-01 10:16:20 +0300

    synchronized

--- java-core/src/main/java/org/sergei/core/threads/Synchronized.java
+++ java-core/src/main/java/org/sergei/core/threads/Synchronized.java
@@ -1,4 +1,65 @@
 package org.sergei.core.threads;
 
+import java.util.ArrayList;
+import java.util.List;
+
+class ListWriter {
+
+    private final List<String> elements;
+
+    ListWriter() {
+        elements = new ArrayList<>();
+    }
+
+    void write(String message) {
+        try {
+            Thread.sleep(3000);
+            for (int i = 0; i < 30; i++) {
+                elements.add(message);
+            }
+            elements.forEach(System.out::println);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+class ListWriterWrite extends Thread {
+
+    private String message;
+    private final ListWriter writer;
+
+    ListWriterWrite(String message, ListWriter writer) {
+        this.message = message;
+        this.writer = writer;
+    }
+
+    @Override
+    public void run() {
+        synchronized (writer) {
+            writer.write(message);
+        }
+    }
+}
+
 public class Synchronized {
+
+    public static void main(String[] args) {
+        ListWriter writer = new ListWriter();
+
+        ListWriterWrite writeOne = new ListWriterWrite("Hello one", writer);
+        ListWriterWrite writeTwo = new ListWriterWrite("Hello two", writer);
+
+        writeOne.start();
+        writeTwo.start();
+
+        try {
+            writeOne.join();
+            writeTwo.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+
 }
