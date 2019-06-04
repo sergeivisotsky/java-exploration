1fc1299bad7898049e78e08d6f68f67cc1c4bde1
Sergei Visotsky <sergei.visotsky@gmail.com>
2019-06-04 22:07:30 +0300

    Hashtable

---
+++ collections/src/main/java/org/sergei/collections/HashtableSample.java
@@ -0,0 +1,16 @@
+package org.sergei.collections;
+
+import java.util.Hashtable;
+
+public class HashtableSample {
+
+    public static void main(String[] args) {
+
+        Hashtable<Integer, String> hashtable = new Hashtable<>();
+        hashtable.put(1, "First element");
+        hashtable.put(2, "Second element");
+        hashtable.put(3, "Third element");
+
+    }
+
+}
