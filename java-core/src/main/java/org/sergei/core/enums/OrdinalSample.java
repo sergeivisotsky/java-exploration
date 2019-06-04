974fc67e367f0d860896df130aae251798adec3f
Sergei Visotsky <sergei.visotsky@gmail.com>
2019-06-04 17:57:11 +0300

    Ordinal method ue case

--- java-core/src/main/java/org/sergei/core/enums/OrdinalSample.java
+++ java-core/src/main/java/org/sergei/core/enums/OrdinalSample.java
@@ -8,7 +8,7 @@ public class OrdinalSample {
 
     public static void main(String[] args) {
         for (Vegetables vegetable : Vegetables.values()) {
-            System.out.println(vegetable);
+            System.out.println(vegetable.ordinal());
         }
     }
 
