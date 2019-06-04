8a9ad35c27aac862dc6ed28a3c5ab8828094a36f
Sergei Visotsky <sergei.visotsky@gmail.com>
2019-06-04 22:06:59 +0300

    Guava collections added

---
+++ collections/src/main/java/org/sergei/collections/guva/LinkedListMethodSample.java
@@ -0,0 +1,31 @@
+package org.sergei.collections.guva;
+
+import com.google.common.collect.Lists;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LinkedListMethodSample {
+
+    public static void main(String[] args) {
+
+        ArrayList<Integer> guavaList = Lists.newArrayList();
+        long startGL = System.nanoTime();
+        for (int i = 0; i < 100_000; i++) {
+            guavaList.add(i);
+        }
+        long endGL = System.nanoTime();
+        System.out.println("Guava ArrayList performance: " + (endGL - startGL));
+
+        List<Integer> list = new ArrayList<>();
+        long startList = System.nanoTime();
+        for (int i = 0; i < 100_000; i++) {
+            list.add(i);
+        }
+        long endList = System.nanoTime();
+        System.out.println("Java collections ArrayList performance: " + (endList - startList));
+
+
+    }
+
+}
