40793ea03809a3a82d452f43f56ad2ea9ac23611
Sergei Visotsky <sergei.visotsky@gmail.com>
2019-06-06 18:22:00 +0300

    Typo

--- io-nio/src/main/java/org/sergei/inout/paths/PathBetweenPaths.java
+++ io-nio/src/main/java/org/sergei/inout/paths/PathBetweenPaths.java
@@ -3,12 +3,12 @@ package org.sergei.inout.paths;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-public class PathBetwennPaths {
+public class PathBetweenPaths {
 
     public static void main(String[] args) {
         Path pOne = Paths.get("src/main/resources/elements");
         Path pTwo = Paths.get("src/main/resources/elements/organisms");
-        
+
         Path pOneToPTwo = pOne.relativize(pTwo);
     }
     
