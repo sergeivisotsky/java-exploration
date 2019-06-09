2a5d9cab947255fe6e4c5c74335c792a3bcb5a64
Sergei Visotsky <sergei.visotsky@gmail.com>
2019-06-09 16:11:25 +0300

    Streams

--- java-core/src/main/java/org/sergei/core/streams/EnableCompletedSample.java
+++ java-core/src/main/java/org/sergei/core/streams/EnableCompletedSample.java
@@ -10,7 +10,7 @@ public class EnableCompletedSample {
     public static void main(String[] args) {
 
         var lst = Arrays.asList(1, 2, 3, 4);
-//        var lst = List.of(1, 2, 3, 4); // Incorrect by itself
+//        var lst = List.of(1, 2, 3, 4); // Incorrect by itself - throws UnsupportedOperationException
         lst.replaceAll(x -> x + 100);
         System.out.println("-Completed-");
 
